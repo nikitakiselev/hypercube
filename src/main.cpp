6f48314bbabde6295446fc1637be9f09be4a2431
Nikita Kiselev <kiselev2008@gmail.com>
2024-10-17 21:56:19 +0300

    add deep sleep mode for esp32

--- src/main.cpp
+++ src/main.cpp
@@ -35,12 +35,36 @@ bool connectToWifi() {
 #endif
 }
 
+/*
+  Method to print the reason by which ESP32
+  has been awaken from sleep
+*/
+void print_wakeup_reason() {
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch (wakeup_reason) {
+    case ESP_SLEEP_WAKEUP_EXT0:     Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1:     Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER:    Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD: Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP:      Serial.println("Wakeup caused by ULP program"); break;
+    default:                        Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason); break;
+  }
+}
+
 void setup() {
   Serial.begin(115200);
 
   Serial.println();
   Serial.println();
   Serial.println("======== Arduino Hyper Cube ========");
+
+  print_wakeup_reason();
+  esp_sleep_enable_ext0_wakeup(BTN_PIN, 0); // 1 = High, 0 = Low
+  rtc_gpio_pullup_dis(BTN_PIN);
+  rtc_gpio_pulldown_en(BTN_PIN);
 
   // analogReadResolution(10); // 10 bit, from 0 to 1023
   // analogSetWidth(10);
@@ -83,6 +107,10 @@ void loop() {
 
     Serial.printf("Current power state: %s\n", settings.powerState ? "On" : "Off");
     digitalWrite(TRANSISTOR_PIN, settings.powerState);
+
+    if (settings.powerState == false) {
+      esp_deep_sleep_start();
+    }
   }
 
   if (btn.hold()) {
